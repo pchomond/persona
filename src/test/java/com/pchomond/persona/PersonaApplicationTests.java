a0d691f3a64408b9a166fad4741013dd82ba02cc
Paschalis Chomondozlis <paschalis.chomondozlis@gmail.com>
2026-04-06 17:23:49 +0300

    feat: initial baseline for persona project

---
+++ src/test/java/com/pchomond/persona/PersonaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pchomond.persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
