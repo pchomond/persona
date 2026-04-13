a9b335ba72cb0938ea18035bf8f0bbc30c815190
Paschalis Chomondozlis <paschalis.chomondozlis@gmail.com>
2026-04-13 23:49:23 +0300

    feat: add negative test, alter tests to not mock data objects

--- src/test/java/com/pchomond/persona/service/UserServiceTest.java
+++ src/test/java/com/pchomond/persona/service/UserServiceTest.java
@@ -2,19 +2,29 @@ package com.pchomond.persona.service;
 
 import com.pchomond.persona.mapper.UserMapper;
 import com.pchomond.persona.model.UserEntity;
+import com.pchomond.persona.model.UserEntity.UserAddress;
 import com.pchomond.persona.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.openapitools.model.Address;
+import org.openapitools.model.BirthDate;
 import org.openapitools.model.CreateUserRequest;
 import org.openapitools.model.User;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -29,18 +39,107 @@ public class UserServiceTest {
     private UserService userService;
 
     @Test
-    void givenCreateUserRequest_shouldCreateAndValidateUser() {
+    void createAndValidateUser_shouldReturnUser_whenNoErrorsOccur() {
         // given
-        var createUserRequest = mock(CreateUserRequest.class);
-        var mockedUserEntity = mock(UserEntity.class);
-        given(userMapper.toUserEntity(eq(createUserRequest))).willReturn(mockedUserEntity);
-        given(userRepository.save(mockedUserEntity)).willReturn(mockedUserEntity);
-        given(userMapper.toUser(mockedUserEntity)).willReturn(mock(User.class));
+        var createUserRequest = generateCreateUserRequest();
+        var mappedUserEntity = generateUserEntity();
+        var persistedUserEntity = generateUserEntity();
+        var expectedUser = generateUser();
+
+        given(userMapper.toUserEntity(eq(createUserRequest))).willReturn(mappedUserEntity);
+        given(userRepository.save(mappedUserEntity)).willReturn(persistedUserEntity);
+        given(userMapper.toUser(persistedUserEntity)).willReturn(expectedUser);
 
         // when
         var result = userService.createAndValidateUser(createUserRequest);
 
         // then
-        assertThat(result).isExactlyInstanceOf(User.class);
+        assertThat(result).isNotNull().isExactlyInstanceOf(User.class);
+
+        verify(userMapper).toUserEntity(createUserRequest);
+        verify(userRepository).save(mappedUserEntity);
+        verify(userMapper).toUser(persistedUserEntity);
+
+        verifyNoMoreInteractions(userMapper, userRepository);
+    }
+
+    @Test
+    void createAndValidateUser_shouldThrowException_whenRepositoryFails() {
+        // given
+        CreateUserRequest createUserRequest = generateCreateUserRequest();
+        UserEntity mappedUserEntity = generateUserEntity();
+
+        when(userMapper.toUserEntity(createUserRequest)).thenReturn(mappedUserEntity);
+
+        RuntimeException dbException = new RuntimeException("Database down");
+        when(userRepository.save(any(UserEntity.class))).thenThrow(dbException);
+
+        // when & then
+        assertThatThrownBy(() -> userService.createAndValidateUser(createUserRequest))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("Database down");
+
+        verify(userMapper).toUserEntity(createUserRequest);
+        verify(userRepository).save(mappedUserEntity);
+
+        verifyNoMoreInteractions(userMapper, userRepository);
+    }
+
+    private static CreateUserRequest generateCreateUserRequest() {
+        return CreateUserRequest.builder()
+                .email("test@gmail.com")
+                .givenName("Test")
+                .surname("User")
+                .dateOfBirth(BirthDate.builder()
+                        .day(2)
+                        .month(1)
+                        .year(1990)
+                        .build())
+                .address(Address.builder()
+                        .line1("Abbey Road 1")
+                        .city("Brentford")
+                        .region("London")
+                        .country("UK")
+                        .postalCode("12341")
+                        .build())
+                .build();
+    }
+
+    private static UserEntity generateUserEntity() {
+        return UserEntity.builder()
+                .userId(UUID.randomUUID())
+                .email("test@gmail.com")
+                .givenName("Test")
+                .surname("User")
+                .dateOfBirth(LocalDate.of(1990, 1, 2))
+                .address(UserAddress.builder()
+                        .line1("Abbey Road 1")
+                        .city("Brentford")
+                        .region("London")
+                        .country("UK")
+                        .postalCode("12341")
+                        .build())
+                .build();
+    }
+
+    private static User generateUser() {
+        return User.builder()
+                .id(UUID.randomUUID().toString())
+                .email("test@gmail.com")
+                .givenName("Test")
+                .surname("User")
+                .dateOfBirth(BirthDate.builder()
+                        .day(2)
+                        .month(1)
+                        .year(1990)
+                        .build())
+                .address(Address.builder()
+                        .line1("Abbey Road 1")
+                        .city("Brentford")
+                        .region("London")
+                        .country("UK")
+                        .postalCode("12341")
+                        .build())
+                .build();
     }
 }
